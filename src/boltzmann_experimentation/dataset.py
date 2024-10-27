640199920ae69caf412ccd3cce32424616d09165
Noa Onoszko <noaono@hotmail.com>
2024-10-27 14:17:16 +0100

    Remove augmentation from CIFAR-10 validation data
    For all models except `deit-b`.

--- src/boltzmann_experimentation/dataset.py
+++ src/boltzmann_experimentation/dataset.py
@@ -14,7 +14,7 @@ class DatasetFactory:
             case "simple-cnn" | "resnet18" | "densenet" | "deit-b":
                 # Define data transformation (e.g., normalization)
                 if model_type == "deit-b":
-                    transform = create_transform(
+                    train_transform = create_transform(
                         input_size=224,  # Resize CIFAR-10 images to 224x224
                         is_training=True,
                         auto_augment="rand-m9-mstd0.5-inc1",
@@ -22,11 +22,11 @@ class DatasetFactory:
                         mean=(0.485, 0.456, 0.406),
                         std=(0.229, 0.224, 0.225),
                     )
+                    val_transform = train_transform
                 else:
-                    transform = transforms.Compose(
+                    # Define common transforms
+                    val_transform = transforms.Compose(
                         [
-                            transforms.RandomHorizontalFlip(),
-                            transforms.RandomCrop(32, padding=4),
                             transforms.ToTensor(),
                             transforms.Normalize(
                                 (0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)
@@ -34,11 +34,20 @@ class DatasetFactory:
                         ]
                     )
 
+                    # Training set transformations with data augmentation
+                    train_transform = transforms.Compose(
+                        [
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomCrop(32, padding=4),
+                            val_transform,  # Apply base transform at the end
+                        ]
+                    )
+
                 train_dataset = datasets.CIFAR10(
-                    root="./data", train=True, download=True, transform=transform
+                    root="./data", train=True, download=True, transform=train_transform
                 )
                 val_dataset = datasets.CIFAR10(
-                    root="./data", train=False, download=True, transform=transform
+                    root="./data", train=False, download=True, transform=val_transform
                 )
                 return train_dataset, val_dataset
             case "two-layer-perceptron" | "single-neuron-perceptron":
