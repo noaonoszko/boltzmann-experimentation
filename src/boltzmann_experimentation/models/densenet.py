84cd70f4a5a36d6aee0bf922acb6ec12e33b07b4
Noa Onoszko <noaono@hotmail.com>
2024-10-31 09:36:58 +0100

    Steal torchvision's `_DenseBlock` and  `_DenseLayer`
    And put batch normalization behind a flag.

--- src/boltzmann_experimentation/models/densenet.py
+++ src/boltzmann_experimentation/models/densenet.py
@@ -1,11 +1,11 @@
 import torch
+from torch import Tensor
 import torch.nn.functional as F
 import torch.nn as nn
 
 from boltzmann_experimentation.config.settings import (
     general_settings as g,
 )
-from torchvision.models.densenet import _DenseBlock
 from torchvision.utils import _log_api_usage_once
 from collections import OrderedDict
 
@@ -104,7 +104,7 @@ class DenseNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         features = self.features(x)
         out = F.relu(features, inplace=True)
         out = F.adaptive_avg_pool2d(out, (1, 1))
@@ -126,3 +126,78 @@ class _Transition(nn.Sequential):
             num_input_features, num_output_features, kernel_size=1, stride=1, bias=False
         )
         self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+
+
+class _DenseLayer(nn.Module):
+    def __init__(
+        self,
+        num_input_features: int,
+        growth_rate: int,
+        bn_size: int,
+        drop_rate: float,
+        memory_efficient: bool = False,
+    ) -> None:
+        super().__init__()
+        self.norm1 = (
+            nn.BatchNorm2d(num_input_features)
+            if g.model_kwargs.get("batch_norm")
+            else nn.Identity()
+        )
+
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(
+            num_input_features,
+            bn_size * growth_rate,
+            kernel_size=1,
+            stride=1,
+            bias=False,
+        )
+
+        self.norm2 = (
+            nn.BatchNorm2d(bn_size * growth_rate)
+            if g.model_kwargs.get("batch_norm")
+            else nn.Identity()
+        )
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(
+            bn_size * growth_rate,
+            growth_rate,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+        )
+
+        self.drop_rate = float(drop_rate)
+        self.memory_efficient = memory_efficient
+
+
+class _DenseBlock(nn.ModuleDict):
+    _version = 2
+
+    def __init__(
+        self,
+        num_layers: int,
+        num_input_features: int,
+        bn_size: int,
+        growth_rate: int,
+        drop_rate: float,
+        memory_efficient: bool = False,
+    ) -> None:
+        super().__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient,
+            )
+            self.add_module("denselayer%d" % (i + 1), layer)
+
+    def forward(self, init_features: Tensor) -> Tensor:
+        features = [init_features]
+        for name, layer in self.items():
+            new_features = layer(features)
+            features.append(new_features)
+        return torch.cat(features, 1)
