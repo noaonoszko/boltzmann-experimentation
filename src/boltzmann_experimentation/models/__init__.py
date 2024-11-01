a0bc2b8f7b51364282bc0c5c1e9d24bcc18bbde4
Noa Onoszko <noaono@hotmail.com>
2024-11-01 20:37:16 +0100

    Remove kuangliu from densenet name

--- src/boltzmann_experimentation/models/__init__.py
+++ src/boltzmann_experimentation/models/__init__.py
@@ -1,7 +1,7 @@
 from .simple_cnn import SimpleCNN
 from .two_layer_perceptron import TwoLayerPerceptron
 from .sinle_layer_perceptron import SingleNeuronPerceptron
-from .densenet_kuangliu import DenseNet121
+from .densenet import DenseNet121
 
 __all__ = [
     "SimpleCNN",
